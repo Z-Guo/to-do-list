34139f9f079b297ecc0f4793a1f1988e965b62d4
Ziling Guo <ziling.guo@redington.co.uk>
2022-08-31 11:37:40 +0100

    delete item

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const todoList = document.querySelector(".todo-list");
 
 
 //event listeners
-todoButton.addEventListener("click", addTodo)
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 // functions 
 
@@ -17,9 +18,9 @@ function addTodo(event) {
     todoDiv.classList.add("todo")
     //create LI
     const newTodo = document.createElement('li')
-    newTodo.innerText = 'hey'
-    newTodo.classList.add('todo-item')
-    todoDiv.appendChild(newTodo)
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
     //check mark button 
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class="fas fa-check"> </i>';
@@ -28,9 +29,25 @@ function addTodo(event) {
     //check delete button 
     const deleteButton = document.createElement('button');
     deleteButton.innerHTML = '<i class = "fas fa-trash"> </i>';
-    deleteButton.classList.add("delete-btn");
+    deleteButton.classList.add("trash-btn");
     todoDiv.appendChild(deleteButton);
     //append to list
     todoList.appendChild(todoDiv);
+    //clear todo input value
+    todoInput.value = "";
+}
+
+function deleteCheck(event) {
+    const item = event.target
+    console.log(event.target)
+    console.log(item.classList)
+    //delete to do
+    if (item.classList[0] === 'trash-btn') {
+        //! instead of removing item, remove the parent element of the item
+        const todo = item.parentElement;
+        console.log(todo)
+        todo.remove()
+    }
+
 }
 
